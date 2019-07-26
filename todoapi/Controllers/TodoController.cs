e7f02a7377303624828ab4014180c1851bef8ef5
Shanbhag <uday.shanbhag@philips.com>
2019-07-26 12:34:45 +0530

    first version

---
+++ todoapi/Controllers/TodoController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using todoapi.Models;
+
+namespace todoapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        // GET: api/Todo
+        [HttpGet]
+        public IEnumerable<Todo> Get()
+        {
+            return new Todo[] { new Todo { Name = "rent", Description = "Pay Rent", CreatedDateTime = DateTime.UtcNow, ModifiedDateTime = DateTime.UtcNow } };
+        }
+
+        // GET: api/Todo/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Todo
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Todo/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ todoapi/Models/Todo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace todoapi.Models
+{
+    public class Todo
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime CreatedDateTime { get; set; }
+
+        public DateTime  ModifiedDateTime { get; set; }
+
+        public User CreatedBy { get; set; }
+    }
+}
