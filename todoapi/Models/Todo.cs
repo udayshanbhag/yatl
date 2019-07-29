53f791c93be650106d91d2b5ba372423f63ce35e
Shanbhag, Uday <uday.shanbhag@philips.com>
2019-07-29 12:09:57 +0530

    working

--- todoapi/Models/Todo.cs
+++ todoapi/Models/Todo.cs
@@ -4,17 +4,51 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace todoapi.Models
-{
+{    
+/// <summary>
+/// 
+/// </summary>
     public class Todo
-    {
-        public string Name { get; set; }
+    {        
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        public string Name { get; set; }        
 
+        /// <summary>
+        /// Gets or sets the description.
+        /// </summary>
+        /// <value>
+        /// The description.
+        /// </value>
         public string Description { get; set; }
-
+        
+        /// <summary>
+        /// Gets or sets the created date time.
+        /// </summary>
+        /// <value>
+        /// The created date time.
+        /// </value>
         public DateTime CreatedDateTime { get; set; }
-
+        
+        /// <summary>
+        /// Gets or sets the modified date time.
+        /// </summary>
+        /// <value>
+        /// The modified date time.
+        /// </value>
         public DateTime  ModifiedDateTime { get; set; }
-
+        
+        /// <summary>
+        /// Gets or sets the created by.
+        /// </summary>
+        /// <value>
+        /// The created by.
+        /// </value>
         public User CreatedBy { get; set; }
+
     }
 }
--- todoapi/Models/User.cs
+++ todoapi/Models/User.cs
@@ -4,9 +4,18 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace todoapi.Models
-{
+{    
+/// <summary>
+/// 
+/// </summary>
     public class User
-    {
+    {        
+        /// <summary>
+        /// Gets or sets the username.
+        /// </summary>
+        /// <value>
+        /// The username.
+        /// </value>
         public string Username { get; set; }
     }
 }
